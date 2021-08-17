d9f6ec1728e52163939218e5ef4edc93a9afed43
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-08-17 16:31:51 -0500

    :hammer: Add hard-clipping workaround

--- bin/jev-function-lib.jl
+++ bin/jev-function-lib.jl
@@ -15,7 +15,7 @@ using XAM
 Get the base at reference position `pos` present in the sequence of `record`.
 """
 function baseatreferenceposition(record::BAM.Record, pos::Int)
-    return BAM.sequence(record)[ref2seq(BAM.alignment(record), pos)[1]]
+    return BAM.sequence(record)[myref2seq(BAM.alignment(record), pos)[1]]
 end # function
 
 """
@@ -24,7 +24,7 @@ end # function
 Get the base at reference position `pos` present in the sequence of `record`.
 """
 function baseatreferenceposition(record::SAM.Record, pos::Int)
-    return SAM.sequence(record)[ref2seq(SAM.alignment(record), pos)[1]]
+    return SAM.sequence(record)[myref2seq(SAM.alignment(record), pos)[1]]
 end # function
 
 struct Variant
@@ -56,3 +56,49 @@ function Variant(data::DataFrameRow)
     return Variant(region, pos, refseq, altseq)
 
 end # function
+
+"""
+    myref2seq(aln::Alignment, i::Int)
+
+Replicates the functionality of BioAlignments `ref2seq`, but can handle hard clips
+by effectively removing them for the intent of finding the position.
+"""
+function myref2seq(aln::Alignment, i::Int)
+    if aln.anchors[2].op == OP_HARD_CLIP
+        # Hard clipping was shown on operation 2
+        # (operation 1 is always a start position)
+
+        # Save where the clipping ends
+        alnstart = aln.anchors[2]
+
+        # Declare a new empty array where we can rebuild the alignment
+        newanchors = AlignmentAnchor[]
+
+        # Rebase the start of our new alignment to where the clipping ends
+        push!(newanchors, AlignmentAnchor(
+            0,
+            aln.anchors[1].refpos,
+            OP_START
+        ))
+
+        # Add new anchors
+        for j in 3:(length(aln.anchors)-1)
+            newanchor = AlignmentAnchor(
+                aln.anchors[j].seqpos - alnstart.seqpos,
+                aln.anchors[j].refpos,
+                aln.anchors[j].op
+            )
+            push!(newanchors, newanchor)
+        end #for
+
+        # Package up our new alignment
+        workingalignment = Alignment(newanchors)
+    else
+        # Package up the old alignment if there was no hard clipping
+        workingalignment = aln
+    end #if
+
+    # Perform regular alignment search, minus any hard clipping
+    return ref2seq(workingalignment, i)
+
+end #function
