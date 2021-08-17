b37f962a9abe0f1c9ead79802d89abe327503448
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-08-17 15:44:18 -0500

    :art::racehorse: Pull variant statistics before making combos and use vector operations for read matching

--- bin/jev-function-lib.jl
+++ bin/jev-function-lib.jl
@@ -3,8 +3,11 @@
 #    include("jev-function-lib.jl")
 
 # Package imports
-using XAM
 using BioAlignments
+using BioSequences
+using BioSymbols
+using DataFrames
+using XAM
 
 """
     baseatreferenceposition(record::BAM.Record, pos::Int)
@@ -22,4 +25,34 @@ Get the base at reference position `pos` present in the sequence of `record`.
 """
 function baseatreferenceposition(record::SAM.Record, pos::Int)
     return SAM.sequence(record)[ref2seq(SAM.alignment(record), pos)[1]]
+end # function
+
+struct Variant
+    region::String
+    position::Int
+    referencebase::NucleotideSeq
+    alternatebase::NucleotideSeq
+end
+
+function Variant(data::DataFrameRow)
+    region = data.REGION
+    pos = data.POS
+    refbase = data.REF
+    altbase = data.ALT
+
+    # Check for insertion
+    if first(altbase) == '+'
+        altbase = string(refbase, altbase[2:end])
+    end # if
+
+    # Check for deletion
+    if first(altbase) == '-'
+        altbase = "-"
+    end
+
+    refseq = LongDNASeq(refbase)
+    altseq = LongDNASeq(altbase)
+
+    return Variant(region, pos, refseq, altseq)
+
 end # function
