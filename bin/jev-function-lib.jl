9357e60273953b064e162713150cd60b9e999b14
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-08-19 12:08:47 -0500

    :art: Clean up documentation and docstrings

--- bin/jev-function-lib.jl
+++ bin/jev-function-lib.jl
@@ -104,6 +104,15 @@ function myref2seq(aln::Alignment, i::Int)
 
 end #function
 
+"""
+    matchvariant(base::Union{NucleotideSeq,DNA,AbstractVector{DNA}}, var::Variant)
+
+Checks if `base` matches the reference or variant expected in `var`, and returns a symbol
+indicating which, if any, it matches.
+
+Returned values can be `:reference` for a reference match, `:alternate` for an alternate
+match, or `:other` for no match with the given variant.
+"""
 function matchvariant(base::NucleotideSeq, var::Variant)
     refbase = LongDNASeq(var.referencebase)
     altbase = LongDNASeq(var.alternatebase)
@@ -125,9 +134,15 @@ function matchvariant(base::AbstractVector{DNA}, var::Variant)
     return matchvariant(LongDNASeq(base), var)
 end
 
-function findvariantlinkages(variantcombos::Vector{Vector{Variant}}, reader::BAM.Reader)
-    # Declare an empty dataframe for the possible combinations
-    variantcombodata = DataFrame(
+"""
+    haplotypeoccurances(variantparings::Vector{Vector{Variant}}, reader::BAM.Reader)
+
+Tabulates the number of types each haplotype corresponding to the reference/alternate
+pairings present in `variantpairings` occurs in the reads contained in `reader`.
+"""
+function haplotypeoccurances(variantparings::Vector{Vector{Variant}}, reader::BAM.Reader)
+    # Declare an empty dataframe for the possible haplotypes
+    haplotypedata = DataFrame(
         Position_1 = Int[],
         Position_2 = Int[],
         Call_1 = String[],
@@ -138,33 +153,32 @@ function findvariantlinkages(variantcombos::Vector{Vector{Variant}}, reader::BAM
         Alternate_Alternate = Int[]
     )
 
-
     # Find the reads that match every possible combo
-    for variantpair in variantcombos
-        # Set up variant combo counts
+    for varpair in variantparings
+        # Set up haplotype counts
         ref_ref = 0
         ref_alt = 0
         alt_ref = 0
         alt_alt = 0
 
         # Get the reads that contain the first variant position
-        containingreads = collect(eachoverlap(reader, variantpair[1].region, variantpair[1].position:variantpair[1].position))
+        containingreads = collect(eachoverlap(reader, varpair[1].region, varpair[1].position:varpair[1].position))
 
         # Get the reads that contain the second variant position
-        containingreads = containingreads[BAM.rightposition.(containingreads) .>= variantpair[2].position]
+        containingreads = containingreads[BAM.rightposition.(containingreads) .>= varpair[2].position]
 
         # Check every NGS read that contains both positions
         for record in containingreads
             # Find this read's basecalls for both locations
             try
-                base1 = baseatreferenceposition(record, variantpair[1].position)
-                base2 = baseatreferenceposition(record, variantpair[2].position)
+                base1 = baseatreferenceposition(record, varpair[1].position)
+                base2 = baseatreferenceposition(record, varpair[2].position)
 
                 # Find how the basecalls stack up
-                match1 = matchvariant(base1, variantpair[1])
-                match2 = matchvariant(base2, variantpair[2])
+                match1 = matchvariant(base1, varpair[1])
+                match2 = matchvariant(base2, varpair[2])
 
-                # Find which combination we're dealing with, if any
+                # Find which haplotype we're dealing with, if any
                 if     match1 == :reference && match2 == :reference
                     ref_ref += 1
                 elseif match1 == :reference && match2 == :alternate
@@ -179,29 +193,37 @@ function findvariantlinkages(variantcombos::Vector{Vector{Variant}}, reader::BAM
             end #try
         end #for
 
-        # Write which combos we have to the overall dataframe
+        # Write the haplotype counts to the overall dataframe
         if sum([ref_ref, alt_ref, ref_alt, alt_alt]) > 0
-            push!(variantcombodata, [variantpair[1].position, variantpair[2].position, variantpair[1].alternatebase, variantpair[2].alternatebase, ref_ref, ref_alt, alt_ref, alt_alt])
+            push!(haplotypedata, [varpair[1].position, varpair[2].position, varpair[1].alternatebase, varpair[2].alternatebase, ref_ref, ref_alt, alt_ref, alt_alt])
         end #if
     end #for
 
-    return variantcombodata
+    return haplotypedata
 end #function
 
-function appendlinkagestatistics!(var_combos::DataFrame)
+"""
+    appendlinkagestatistics!(haplotypecounts::DataFrame)
+
+Calculates linkage disequilibrium and the Χ-squared p-value of linkage disequilibrium for
+haplotypes contained in `haplotypecounts`, filtering out rows where it is invalid to
+calculate linkage disequilibrium. `haplotypecounts` is expected to be in the same format
+returned by [`haplotypeoccurances`](@ref).
+"""
+function appendlinkagestatistics!(haplotypecounts::DataFrame)
     # Filter out problem cases
     # 1. Filter out instances where linkage disequilibrium can't be calculated (0 instances of an allele combo)
-    filter!(v -> prod([v.Reference_Reference, v.Reference_Alternate, v.Alternate_Reference, v.Alternate_Alternate]) > 0, var_combos)
+    filter!(v -> prod([v.Reference_Reference, v.Reference_Alternate, v.Alternate_Reference, v.Alternate_Alternate]) > 0, haplotypecounts)
     # 2. Filter out low read depth instances (< 100 total reads)
-    filter!(v -> sum([v.Reference_Reference, v.Reference_Alternate, v.Alternate_Reference, v.Alternate_Alternate]) < 100, var_combos)
+    filter!(v -> sum([v.Reference_Reference, v.Reference_Alternate, v.Alternate_Reference, v.Alternate_Alternate]) < 100, haplotypecounts)
     # 3. Filter out low double-mutation instances (< 5 alt_alt reads)
-    filter!(v -> v.Alternate_Alternate < 5, var_combos)
+    filter!(v -> v.Alternate_Alternate < 5, haplotypecounts)
 
     # Split out the fields into managable variables
-    ref_ref = var_combos.Reference_Reference
-    ref_alt = var_combos.Reference_Alternate
-    alt_ref = var_combos.Alternate_Reference
-    alt_alt = var_combos.Alternate_Alternate
+    ref_ref = haplotypecounts.Reference_Reference
+    ref_alt = haplotypecounts.Reference_Alternate
+    alt_ref = haplotypecounts.Alternate_Reference
+    alt_alt = haplotypecounts.Alternate_Alternate
 
     # Tabulate allele frequencies
     total = ref_ref .+ ref_alt .+ alt_ref .+ alt_alt
@@ -235,8 +257,8 @@ function appendlinkagestatistics!(var_combos::DataFrame)
     sig = 1 .- cdf.(Chisq(1), linkagestatistic)
 
     # Add linkage and significance to the dataframe
-    var_combos.Linkage_Disequilibrium = linkage
-    var_combos.Linkage_Significance = sig
+    haplotypecounts.Linkage_Disequilibrium = linkage
+    haplotypecounts.Linkage_Significance = sig
 
-    return var_combos
+    return haplotypecounts
 end
