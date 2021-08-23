cb2b3284cc9d188f4f0159d7f974553a8ede5470
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-08-23 15:21:39 -0500

    :arrows_clockwise: Switch to using type-based function calls instead of dataframe manipulation

--- bin/jev-function-lib.jl
+++ bin/jev-function-lib.jl
@@ -54,16 +54,28 @@ function Variant(data::DataFrameRow)
 
 end # function
 
+function region(v::Variant)
+    return v.region
+end
+
+function Base.position(v::Variant)
+    return v.position
+end
+
+function referencebase(v::Variant)
+    return v.referencebase
+end
+
+function alternatebase(v::Variant)
+    return v.alternatebase
+end
+
 struct Haplotype
     mutations::AbstractVector{Variant}
 end
 
 function Haplotype(var::Variant)
     return Haplotype([var])
-end
-
-function Base.position(var::Variant)
-    return var.position
 end
 
 struct HaplotypeCounts
@@ -85,6 +97,34 @@ end
 
 function counts(h::HaplotypeCounts)
     return h.counts
+end
+
+function haplotype(h::HaplotypeCounts)
+    return h.haplotype
+end
+
+function mutations(h::HaplotypeCounts)
+    return h.haplotype.mutations
+end
+
+function mutations(h::Haplotype)
+    return h.mutations
+end
+
+function ref_ref(h::HaplotypeCounts)
+    return h.counts[1,1]
+end
+
+function ref_alt(h::HaplotypeCounts)
+    return h.counts[1,2]
+end
+
+function alt_ref(h::HaplotypeCounts)
+    return h.counts[2,1]
+end
+
+function alt_alt(h::HaplotypeCounts)
+    return h.counts[2,2]
 end
 
 """
@@ -228,6 +268,10 @@ function findoccurrences(haplotype::Haplotype, reads::AbstractVector{BAM.Record}
 
 end
 
+function findoccurrences(haplotype::Haplotype, reads::AbstractVector{BAM.Reader})
+    return findoccurrences(haplotype, collect(reads))
+end
+
 function linkage(counts::AbstractMatrix{Int})
     # Split out the fields into managable variables
     ref_ref = counts[1,1]
@@ -258,130 +302,4 @@ function linkage(counts::AbstractMatrix{Int})
     p = 1 .- cdf.(Chisq(1), Χ_squared)
 
     return Δ, p
-end
-
-
-"""
-    haplotypeoccurances(variantparings::Vector{Vector{Variant}}, reader::BAM.Reader)
-
-Tabulates the number of types each haplotype corresponding to the reference/alternate
-pairings present in `variantpairings` occurs in the reads contained in `reader`.
-"""
-function haplotypeoccurances(variantparings::Vector{Vector{Variant}}, reader::BAM.Reader)
-    # Declare an empty dataframe for the possible haplotypes
-    haplotypedata = DataFrame(
-        Position_1 = Int[],
-        Position_2 = Int[],
-        Call_1 = String[],
-        Call_2 = String[],
-        Reference_Reference = Int[],
-        Reference_Alternate = Int[],
-        Alternate_Reference = Int[],
-        Alternate_Alternate = Int[]
-    )
-
-    # Find the reads that match every possible combo
-    for varpair in variantparings
-        # Set up haplotype counts
-        ref_ref = 0
-        ref_alt = 0
-        alt_ref = 0
-        alt_alt = 0
-
-        # Get the reads that contain the first variant position
-        containingreads = collect(eachoverlap(reader, varpair[1].region, varpair[1].position:varpair[1].position))
-
-        # Get the reads that contain the second variant position
-        containingreads = containingreads[BAM.rightposition.(containingreads) .>= varpair[2].position]
-
-        # Check every NGS read that contains both positions
-        for record in containingreads
-            # Find this read's basecalls for both locations
-            base1 = baseatreferenceposition(record, varpair[1].position)
-            base2 = baseatreferenceposition(record, varpair[2].position)
-
-            # Find how the basecalls stack up
-            match1 = matchvariant(base1, varpair[1])
-            match2 = matchvariant(base2, varpair[2])
-
-            # Find which haplotype we're dealing with, if any
-            if     match1 == :reference && match2 == :reference
-                ref_ref += 1
-            elseif match1 == :reference && match2 == :alternate
-                ref_alt += 1
-            elseif match1 == :alternate && match2 == :reference
-                alt_ref += 1
-            elseif match1 == :alternate && match2 == :alternate
-                alt_alt += 1
-            end #if
-        end #for
-
-        # Write the haplotype counts to the overall dataframe
-        if sum([ref_ref, alt_ref, ref_alt, alt_alt]) > 0
-            push!(haplotypedata, [varpair[1].position, varpair[2].position, varpair[1].alternatebase, varpair[2].alternatebase, ref_ref, ref_alt, alt_ref, alt_alt])
-        end #if
-    end #for
-
-    return haplotypedata
 end #function
-
-"""
-    appendlinkagestatistics!(haplotypecounts::DataFrame)
-
-Calculates linkage disequilibrium and the Χ-squared p-value of linkage disequilibrium for
-haplotypes contained in `haplotypecounts`, filtering out rows where it is invalid to
-calculate linkage disequilibrium. `haplotypecounts` is expected to be in the same format
-returned by [`haplotypeoccurances`](@ref).
-"""
-function appendlinkagestatistics!(haplotypecounts::DataFrame)
-    # Filter out problem cases
-    # 1. Filter out instances where linkage disequilibrium can't be calculated (0 instances of an allele combo)
-    filter!(v -> prod([v.Reference_Reference, v.Reference_Alternate, v.Alternate_Reference, v.Alternate_Alternate]) > 0, haplotypecounts)
-    # 2. Filter out low read depth instances (< 100 total reads)
-    filter!(v -> sum([v.Reference_Reference, v.Reference_Alternate, v.Alternate_Reference, v.Alternate_Alternate]) < 100, haplotypecounts)
-    # 3. Filter out low double-mutation instances (< 5 alt_alt reads)
-    filter!(v -> v.Alternate_Alternate < 5, haplotypecounts)
-
-    # Split out the fields into managable variables
-    ref_ref = haplotypecounts.Reference_Reference
-    ref_alt = haplotypecounts.Reference_Alternate
-    alt_ref = haplotypecounts.Alternate_Reference
-    alt_alt = haplotypecounts.Alternate_Alternate
-
-    # Tabulate allele frequencies
-    total = ref_ref .+ ref_alt .+ alt_ref .+ alt_alt
-    ref_1 = ref_ref .+ ref_alt
-    ref_2 = ref_ref .+ alt_ref
-    alt_1 = alt_ref .+ alt_alt
-    alt_2 = ref_alt .+ alt_alt
-
-    # Calculate allele probabilities (unused figures are not present)
-    p_ref_ref = ref_ref ./ total
-    p_ref_1 = ref_1 ./ total
-    p_ref_2 = ref_2 ./ total
-
-    # Calculate linkage disequilibrium
-    linkage =
-        p_ref_ref .- (p_ref_1 .* p_ref_2)
-
-    # Get the test statstic for linkage disequilibrium
-    Δlinkage = (sqrt.(total .* alt_alt) .- sqrt.(alt_1 .* alt_2)) ./ total
-    linkageerror = (0.5 ./ total) .* sqrt.(
-        ref_ref .- (4 .* total .* Δlinkage .*
-            (
-                ((alt_1 .+ alt_2)./(2 .* sqrt.(alt_1 .* alt_2))) .-
-                (sqrt.(alt_1 .* alt_2) ./ total)
-            )
-        )
-    )
-    linkagestatistic = abs.(Δlinkage ./ linkageerror)
-
-    # Check for significance
-    sig = 1 .- cdf.(Chisq(1), linkagestatistic)
-
-    # Add linkage and significance to the dataframe
-    haplotypecounts.Linkage_Disequilibrium = linkage
-    haplotypecounts.Linkage_Significance = sig
-
-    return haplotypecounts
-end
