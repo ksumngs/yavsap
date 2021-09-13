701a0f25ee2afce502128e83dcf5d00bd5daf925
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-09-13 11:06:00 -0500

    :muscle: Use base62 for haplotype names

--- bin/jev-function-lib.jl
+++ bin/jev-function-lib.jl
@@ -553,7 +553,7 @@ end
 
 function mutaterecord(record::FASTA.Record, haplotype::Haplotype)
     mutationpos = position.(mutations(haplotype))
-    newid = appendedid(record)
+    newid = string(lpad.(base62.(mutationpos),3,'0')...)
     newseq = mutatesequence(sequence(record), haplotype)
     newdesc = ""
     if FASTA.hasdescription(record)
@@ -578,9 +578,77 @@ function mutatesequence(seq::NucleotideSeq, haplotype::Haplotype)
     return newseq
 end
 
-function appendedid(record::FASTA.Record)
-    oldid = identifier(record)
-    accessionnum = first(split(oldid, ".")[1],5)
-    uniqueid = last(string(uuid1()),2)
-    return string(accessionnum, "...", uniqueid)
+BaseLookup = Dict([
+    (0,  '0'),
+    (1,  '1'),
+    (2,  '2'),
+    (3,  '3'),
+    (4,  '4'),
+    (5,  '5'),
+    (6,  '6'),
+    (7,  '7'),
+    (8,  '8'),
+    (9,  '9'),
+    (10, 'a'),
+    (11, 'b'),
+    (12, 'c'),
+    (13, 'd'),
+    (14, 'e'),
+    (15, 'f'),
+    (16, 'g'),
+    (17, 'h'),
+    (18, 'I'),
+    (19, 'j'),
+    (20, 'k'),
+    (21, 'l'),
+    (22, 'm'),
+    (23, 'n'),
+    (24, 'o'),
+    (25, 'p'),
+    (26, 'q'),
+    (27, 'r'),
+    (28, 's'),
+    (29, 't'),
+    (30, 'u'),
+    (31, 'v'),
+    (32, 'w'),
+    (33, 'x'),
+    (34, 'y'),
+    (35, 'z'),
+    (36, 'A'),
+    (37, 'B'),
+    (38, 'C'),
+    (39, 'D'),
+    (40, 'E'),
+    (41, 'F'),
+    (42, 'G'),
+    (43, 'H'),
+    (44, 'I'),
+    (45, 'J'),
+    (46, 'K'),
+    (47, 'L'),
+    (48, 'M'),
+    (49, 'N'),
+    (50, 'O'),
+    (51, 'P'),
+    (52, 'Q'),
+    (53, 'R'),
+    (54, 'S'),
+    (55, 'T'),
+    (56, 'U'),
+    (57, 'V'),
+    (58, 'W'),
+    (59, 'X'),
+    (60, 'Y'),
+    (61, 'Z')
+])
+
+function base62(number::Int)
+    returnstring = Char[]
+    while number >= 1
+        remainder = number % 62
+        pushfirst!(returnstring, BaseLookup[remainder])
+        number = number ÷ 62
+    end
+    return string(returnstring...)
 end
