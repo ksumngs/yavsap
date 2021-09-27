d5a788fc42c1334e1893dbc891afa9ca677278fb
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-09-27 11:07:41 -0500

    organization: Use self-contained visualization generator
    The code used to generate alignment visualizations was hosted on GitHub at
    https://github.com/MillironX/igv-bundler, but that code required multiple
    branches to serve multiple projects and kept related code in different
    locations with no real separation of concerns. Move that code to this repo,
    update the git meta documents to handle the new code type, and update the
    workflow script to copy files directly instead of cloning from Github.
    
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

---
+++ visualizer/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.use('/data', express.static(__dirname + '/data'));
+
+app.get('/samples', function(req, res) {
+    files = fs.readdirSync(path.join(__dirname + '/data'));
+    contig_files = files.filter(file => file.endsWith('.bam'));
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(contig_files));
+})
+
+app.get('/reference', function(req, res) {
+    files = fs.readdirSync(path.join(__dirname + '/data'));
+    fasta_files = files.filter(file => file.endsWith('.fasta'));
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(fasta_files))
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log('running listening on port ' + port)
+})
