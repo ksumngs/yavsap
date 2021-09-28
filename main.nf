ksumngs/yavsap

commit d5a788fc42c1334e1893dbc891afa9ca677278fb

organization: Use self-contained visualization generator
The code used to generate alignment visualizations was hosted on GitHub at
https://github.com/MillironX/igv-bundler, but that code required multiple
branches to serve multiple projects and kept related code in different
locations with no real separation of concerns. Move that code to this repo,
update the git meta documents to handle the new code type, and update the
workflow script to copy files directly instead of cloning from Github.

Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

main.nf
@@ -492,11 +492,7 @@ process presentation_generator {
     """
     mkdir data
     mv *.fasta *.fasta.fai *.bam *.bam.bai data
-    git clone https://github.com/MillironX/igv-bundler.git igv-bundler
-    cd igv-bundler
-    git checkout jev
-    cd ..
-    mv igv-bundler/{index.html,index.js,package.json} .
+    cp ${workflow.projectDir}/visualizer/* .
     """
 }
 

.gitignore
@@ -1,3 +1,4 @@
+### Nextflow gitignore ###
 .nextflow*
 work/
 data/
@@ -58,3 +59,125 @@ docs/site/
 # committed for packages, but should be committed for applications that require a static
 # environment.
 # Manifest.toml
+
+### Node gitignore ###
+# Logs
+logs
+*.log
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+lerna-debug.log*
+.pnpm-debug.log*
+
+# Diagnostic reports (https://nodejs.org/api/report.html)
+report.[0-9]*.[0-9]*.[0-9]*.[0-9]*.json
+
+# Runtime data
+pids
+*.pid
+*.seed
+*.pid.lock
+
+# Directory for instrumented libs generated by jscoverage/JSCover
+lib-cov
+
+# Coverage directory used by tools like istanbul
+coverage
+*.lcov
+
+# nyc test coverage
+.nyc_output
+
+# Grunt intermediate storage (https://gruntjs.com/creating-plugins#storing-task-files)
+.grunt
+
+# Bower dependency directory (https://bower.io/)
+bower_components
+
+# node-waf configuration
+.lock-wscript
+
+# Compiled binary addons (https://nodejs.org/api/addons.html)
+build/Release
+
+# Dependency directories
+node_modules/
+jspm_packages/
+
+# Snowpack dependency directory (https://snowpack.dev/)
+web_modules/
+
+# TypeScript cache
+*.tsbuildinfo
+
+# Optional npm cache directory
+.npm
+
+# Optional eslint cache
+.eslintcache
+
+# Microbundle cache
+.rpt2_cache/
+.rts2_cache_cjs/
+.rts2_cache_es/
+.rts2_cache_umd/
+
+# Optional REPL history
+.node_repl_history
+
+# Output of 'npm pack'
+*.tgz
+
+# Yarn Integrity file
+.yarn-integrity
+
+# dotenv environment variables file
+.env
+.env.test
+.env.production
+
+# parcel-bundler cache (https://parceljs.org/)
+.cache
+.parcel-cache
+
+# Next.js build output
+.next
+out
+
+# Nuxt.js build / generate output
+.nuxt
+dist
+
+# Gatsby files
+.cache/
+# Comment in the public line in if your project uses Gatsby and not Next.js
+# https://nextjs.org/blog/next-9-1#public-directory-support
+# public
+
+# vuepress build output
+.vuepress/dist
+
+# Serverless directories
+.serverless/
+
+# FuseBox cache
+.fusebox/
+
+# DynamoDB Local files
+.dynamodb/
+
+# TernJS port file
+.tern-port
+
+# Stores VSCode versions used for testing VSCode extensions
+.vscode-test
+
+# yarn v2
+.yarn/cache
+.yarn/unplugged
+.yarn/build-state.yml
+.yarn/install-state.gz
+.pnp.*
+
+package-lock.json

visualizer/index.html
@@ -1,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            body {
+                background-color: #d1d1f1;
+            }
+            #viewer {
+                outline: solid;
+                border-radius: 2pt;
+                background-color: white;
+            }
+            h1 {
+                font-family: 'Myriad Pro', 'Noto Sans', 'Lucida Sans', Verdana, 'Liberation Sans', sans-serif;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>JEV Results</h1>
+        <div id="viewer"></div>
+        <!-- <script src="node_modules/igv/dist/igv.js"></script> -->
+        <script src="https://cdn.jsdelivr.net/npm/igv@2.8.6/dist/igv.min.js"></script>
+        <script>
+            var reference
+            var req = new XMLHttpRequest();
+            req.onload = reqlistener;
+            function reqlistener(e) {
+                reference = JSON.parse(this.responseText)[0];
+            }
+            req.open('GET', '/reference', false)
+            req.send()
+
+            var samples
+            var re = new XMLHttpRequest();
+            re.onload = relistener;
+            function relistener(e) {
+                samples = JSON.parse(this.responseText);
+            }
+            re.open('GET', '/samples', false)
+            re.send()
+
+            var trks = [];
+
+            for (var i = 0; i < samples.length; i++) {
+                s = samples[i]
+                trks.push(
+                    {
+                        type: 'alignment',
+                        format: 'bam',
+                        url: 'data/'+s,
+                        indexURL: 'data/'+s+'.bai',
+                        name: s.slice(0,-4)
+                    }
+                )
+            }
+
+            igvDiv = document.getElementById('viewer');
+            options = {
+                reference: {
+                    id: reference.slice(0,-6),
+                    fastaURL: "data/"+reference
+                },
+                tracks: trks
+            }
+            igv.createBrowser(igvDiv, options).
+                then(function(browser) {
+                    igv.browser = browser;
+                });
+        </script>
+    </body>
+</html>

visualizer/index.js
@@ -1,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.use('/data', express.static(__dirname + '/data'));
+
+app.get('/samples', function(req, res) {
+    files = fs.readdirSync(path.join(__dirname + '/data'));
+    contig_files = files.filter(file => file.endsWith('.bam'));
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(contig_files));
+})
+
+app.get('/reference', function(req, res) {
+    files = fs.readdirSync(path.join(__dirname + '/data'));
+    fasta_files = files.filter(file => file.endsWith('.fasta'));
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(fasta_files))
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log('running listening on port ' + port)
+})

commit 1c06db926fcabe5183812d86d9818ad52cfd1256

output, bug: Allow copying from node-initalized presentation directory
If a user runs `npm install` from the project directory (e.g. for debugging
purposes), then `cp` will fail because of the presence of the
`node_modules` directory without the `-r` flag. The goal is to only copy
the output files, so explicitly list them in the command line.

Fixes: d5a788fc42c1 ("organization: Use self-contained visualization generator")
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

main.nf
@@ -494,7 +494,7 @@ process presentation_generator {
     """
     mkdir data
     mv *.fasta *.fasta.fai *.bam *.bam.bai data
-    cp ${workflow.projectDir}/visualizer/* .
+    cp ${workflow.projectDir}/visualizer/{index.html,index.js,package.json} .
     """
 }
 