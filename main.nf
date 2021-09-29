ksumngs/yavsap

commit 85b3cb8cd6c7744a291fc04a47c297650d1261bf

linting: Move modules to 'local' folder
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

main.nf
@@ -56,9 +56,9 @@ if (!params.ont && !params.pe) {
 // Declare what we're going to call our reference genome
 ReferenceName = 'JEV'
 
-include { reference_genome_pull } from './modules/reference.nf'
-include { trimming } from './modules/trimming.nf'
-include { assembly } from './modules/assembly.nf'
+include { reference_genome_pull } from './modules/local/reference.nf'
+include { trimming }              from './modules/local/trimming.nf'
+include { assembly }              from './modules/local/assembly.nf'
 
 cowsay(
 """\

commit ee9b03ba0c1e30c2d2f94336b20eec2e5fe71fe3

linting: Move individual workflows to more appropriately-named folder
Fixes: 85b3cb8cd6c7 ("linting: Move modules to 'local' folder")
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

main.nf
@@ -56,9 +56,9 @@ if (!params.ont && !params.pe) {
 // Declare what we're going to call our reference genome
 ReferenceName = 'JEV'
 
-include { reference_genome_pull } from './modules/local/reference.nf'
-include { trimming }              from './modules/local/trimming.nf'
-include { assembly }              from './modules/local/assembly.nf'
+include { reference_genome_pull } from './subworkflows/reference.nf'
+include { trimming }              from './subworkflows/trimming.nf'
+include { assembly }              from './subworkflows/assembly.nf'
 
 cowsay(
 """\