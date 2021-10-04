ksumngs/yavsap

commit 0fe185fb9cc654b76d48d7fc060fc263028bb3d5

correction: Use all assemblies for phylogenetic analysis
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

subworkflows/haplotype.nf
@@ -168,20 +168,12 @@ process merge_fastas {
     output:
     tuple val(sampleName), file("${sampleName}.population.fasta")
 
-    shell:
-    '''
-    # Keep only the first (most complete) consensus sequence
-    if [ "$(grep -c '^>' !{assembly})" -gt 1 ]; then
-        head !{assembly} -n $(( $(grep -n '^>' !{assembly} | tail -n +2 | head -n1 | awk '{split($0,a,":"); print a[1]}') - 1 )) > consensus.fasta
-    else
-        cp !{assembly} consensus.fasta
-    fi
-
+    script:
+    """
     # Label the consensus sequences as such
-    sed -i "s/>/>!{sampleName}./g" consensus.fasta
-
-    cat !{reference[0]} consensus.fasta !{haplotypes} > !{sampleName}.population.fasta
-    '''
+    sed -i "s/>/>${sampleName}./g" consensus.fasta
+    cat ${reference[0]} consensus.fasta ${haplotypes} > ${sampleName}.population.fasta
+    """
 }
 
 process alignment {

commit 386ddf0d645d1ff955dacdd42cb8053694bde419

Fix filename bug in fasta merging process
Use a consistent and more accurate filename when referring to the
assembled contigs in the merging process.

Fixes: 0fe185fb9cc6 ("correction: Use all assemblies for phylogenetic analysis")
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

subworkflows/haplotype.nf
@@ -170,9 +170,15 @@ process merge_fastas {
 
     script:
     """
-    # Label the consensus sequences as such
-    sed -i "s/>/>${sampleName}./g" consensus.fasta
-    cat ${reference[0]} consensus.fasta ${haplotypes} > ${sampleName}.population.fasta
+    # Create an editable copy of the assembly
+    cp ${assembly} assembly.fasta
+
+    # Ensure the records have unique names by prepending the sample name
+    # This work for contigs generated by SPAdes and Canu
+    sed -i "s/>/>${sampleName}./g" assembly.fasta
+
+    # cat them togther
+    cat ${reference[0]} assembly.fasta ${haplotypes} > ${sampleName}.population.fasta
     """
 }
 