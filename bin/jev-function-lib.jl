ksumngs/yavsap

commit e56899a7de8d886af2411b2b25d9513788daea1e

generalization: Change to N-dimensional linkage disequilibrium calculation
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

bin/jev-function-lib.jl
@@ -10,6 +10,7 @@ using DataFrames
 using Distributions
 using FASTX
 using SHA
+using Statistics
 using XAM
 
 """
@@ -309,37 +310,71 @@ function findoccurrences(haplotype::Haplotype, reads::BAM.Reader)
     return findoccurrences(haplotype, collect(reads))
 end
 
-function linkage(counts::AbstractMatrix{Int})
-    # Split out the fields into managable variables
-    ref_ref = counts[1,1]
-    ref_alt = counts[1,2]
-    alt_ref = counts[2,1]
-    alt_alt = counts[2,2]
+"""
+    linkage(counts::AbstractArray{Int})
 
-    # Tabulate allele frequencies
-    total = ref_ref .+ ref_alt .+ alt_ref .+ alt_alt
-    ref_1 = ref_ref .+ ref_alt
-    ref_2 = ref_ref .+ alt_ref
-    alt_1 = alt_ref .+ alt_alt
-    alt_2 = ref_alt .+ alt_alt
+Calculates the linkage disequilibrium and Chi-squared significance level of a combination of
+haplotypes whose number of occurrences are given by `counts`.
 
-    # Calculate allele probabilities (unused figures are not present)
-    p_ref_ref = ref_ref ./ total
-    p_ref_1 = ref_1 ./ total
-    p_ref_2 = ref_2 ./ total
+`counts` is an ``N``-dimensional array where the ``N``th dimension represents the ``N``th
+variant call position within a haplotype. Position 1 of ``N`` indicates the number of times
+the reference base was called for this variant position, while position 2 indicates the
+number of times the alternate base was called. For example, `counts[1,2,1]` would give the
+number of times the reference was called in position 1, the alternate was also called in
+position 2, and the reference was also called in position 3.
+"""
+function linkage(counts::AbstractArray{Int})
+    # Get the probability of finding a perfect reference sequence
+    P_allref = first(counts) / sum(counts)
+
+    # Get the probabilities of finding reference bases in any of the haplotypes
+    P_refs = sumsliced.([counts], 1:ndims(counts)) ./ sum(instances)
 
     # Calculate linkage disequilibrium
-    Δ = p_ref_ref .- (p_ref_1 .* p_ref_2)
+    Δ = P_allref - prod(P_refs)
 
-    # Get the test statstic for linkage disequilibrium
-    r = Δ / sqrt(p_ref_1 * (1 - p_ref_1) * p_ref_2 * (1 - p_ref_2))
-    Χ_squared = r^2 * total
+    # Calculate the test statistic
+    S = Δ^2 / var(counts)
 
-    # Check for significance
-    p = 1 .- cdf.(Chisq(1), Χ_squared)
+    # Calculate the significance
+    p = 1 - cdf(Chisq(1), S)
 
     return Δ, p
 end #function
+
+"""
+    sumsliced(A::AbstractArray, dim::Int, pos::Int=1)
+
+Sum all elements that are that can be referenced by `pos` in the `dim` dimension of `A`.
+
+# Example
+
+```julia-repl
+julia> A = reshape(1:8, 2, 2, 2)
+2×2×2 reshape(::UnitRange{Int64}, 2, 2, 2) with eltype Int64:
+[:, :, 1] =
+ 1  3
+ 2  4
+
+[:, :, 2] =
+ 5  7
+ 6  8
+
+julia> sumsliced(A, 2)
+16
+
+julia> sumsliced(A, 2, 2)
+20
+```
+
+Heavily inspired by Holy, Tim "Multidimensional algorithms and iteration"
+<https://julialang.org/blog/2016/02/iteration/#filtering_along_a_specified_dimension_exploiting_multiple_indexes>
+"""
+function sumsliced(A::AbstractArray, dim::Int, pos::Int=1)
+    i_pre  = CartesianIndices(size(A)[1:dim-1])
+    i_post = CartesianIndices(size(A)[dim+1:end])
+    return sum(A[i_pre, pos, i_post])
+end
 
 function prop_test(x, n)
     k = length(x)

commit 68b6d08ecec60336e49eef7cc623068985b88e1a

bug: Fix referencing count data by wrong name
Fixes: e56899a7de8d ("generalization: Change to N-dimensional linkage disequilibrium calculation")
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

bin/jev-function-lib.jl
@@ -308,7 +308,7 @@ function linkage(counts::AbstractArray{Int})
     P_allref = first(counts) / sum(counts)
 
     # Get the probabilities of finding reference bases in any of the haplotypes
-    P_refs = sumsliced.([counts], 1:ndims(counts)) ./ sum(instances)
+    P_refs = sumsliced.([counts], 1:ndims(counts)) ./ sum(counts)
 
     # Calculate linkage disequilibrium
     Δ = P_allref - prod(P_refs)