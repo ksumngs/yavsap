ksumngs/yavsap

commit 462e2385f7182f9d2add86dab3f1f9bb664972c7

visualizer: Add rough phylo-io support
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

visualizer/index.js
@@ -13,6 +13,8 @@ app.get('/favicon.ico', function(req, res) {
 
 app.use('/data', express.static(__dirname + '/data'));
 
+app.use('/js', express.static(__dirname + '/js'));
+
 app.get('/samples', function(req, res) {
     files = fs.readdirSync(path.join(__dirname + '/data'));
     contig_files = files.filter(file => file.endsWith('.bam'));

visualizer/js/bcn_processor.js
@@ -1,0 +1,158 @@
+/**
+ * Created by sduvaud on 20/12/16.
+ */
+/**
+ * Created by sduvaud on 14/12/16.
+ */
+importScripts('underscore.min.js');
+
+/*
+ Calculate the Best Corresponding Node (BCN) for all visible nodes (not collapsed) in the tree
+ if recalculate==false, doesn't calculate for a node if it aleady has a value
+ Algorithm adapted from: TreeJuxtaposer: Scalable Tree Comparison Using Focus+Context with Guaranteed Visibility, Munzner et al. 2003
+ */
+function wrapGetAllBCNs(tree1, tree2, recalculate) {
+
+    function getAllBCNs(d, t) {
+
+        //var children = d.children ? d.children : [];
+        var children = getChildren(d);
+        if (children.length > 0) {
+            for (var a = 0; a < children.length; a++) {
+                getAllBCNs(children[a], t);
+            }
+            if (recalculate || !d.elementBCN) {
+                BCN(d, t);
+                // once all the leaves have been compared to the other tree, the node is also compared.
+                // Seems rather redundant, but if we comment this out, the tree has no coloring...
+            }
+            return;
+        } else {
+            if (recalculate || !d.elementBCN) {
+                BCN(d, t);
+            }
+            return;
+        }
+    }
+    getAllBCNs(tree1, tree2);
+    postMessage(tree1);
+}
+/**
+ * Description:
+ *  Get the best corresponding node in opposite tree for node v
+ *  First gets the list of leaves of node v
+ *  Then finds the list of spanning trees (see definition above)
+ *  For each spanning tree, evaluates the similarity with node v
+ *  and assigns the best scoring node to node v as well as a
+ *  similarity score
+ *
+ * Arguments: v is a node
+ *            tree is a tree
+ */
+function BCN(v, tree) {
+
+    var elementBCNNode = null;
+    var maxElementS = 0;
+
+    var spanningTree = getSpanningTree(tree, v);
+
+    for (var i = 0; i < spanningTree.length; i++) {
+        //get elementBCN for node v
+        x = getElementS(v, spanningTree[i]);
+        if (x > maxElementS) {
+            maxElementS = x;
+            elementBCNNode = spanningTree[i];
+        }
+    }
+    v.elementBCN = elementBCNNode;
+    v.elementS = maxElementS;
+}
+
+/*
+ Spanning tree: if a node in the opposite tree is common with a given leaf (same name),
+ then all the nodes are associated to the leaf.
+
+ Example:
+ (A:0.1,B:0.2,(C:0.3,D:0.4):0.5);
+ vs
+ (A:0.1,B:0.2,(C:0.3,D:0.4):0.5);
+ In tree 1, node C,D (0.5) is associated with opposite spanning tree:
+ - Root (length: 0.1 and depth: 0)
+ - C,D  (length: 0.5 and depth: 1)
+ - C    (length: 0.3 and depth: 2)
+ - D    (length: 0.4 and depth: 2)
+
+ Description:
+ Get a spanning tree associated to leaves
+
+ Arguments:
+ - node is set to opposite tree
+ - leaves are searched in opposite tree in order to find the spanning tree
+ */
+function getSpanningTree(tree, node) {
+    var nodes = [];
+    //var bcns = [];
+    for (var i = 0; i < tree.leaves.length; i++) {
+        var test = _.indexOf(node.deepLeafList, tree.leaves[i].name);
+        if (test > -1){
+            nodes.push(tree);
+            //bcns.push(getElementS(tree, node));
+            var children = getChildren(tree);
+            for (var j = 0; j < children.length; j++) {
+                nodes = nodes.concat(getSpanningTree(children[j], node));
+            }
+            return nodes;
+        }
+    }
+    return nodes;
+}
+
+/*
+ Description:
+ Get the comparison score between two nodes
+ First gets all the leaves from the 2 nodes/trees
+ Then get the number of common elements in both lists
+ Computes a score (the higher, the better the comparision)
+
+ Note:
+ Difference between deep leaf list and leaves in:
+ (A:0.1,B:0.2,(C:0.3,D:0.4):0.5);
+ - Root has leaves: A, B, C and D (terminal leaves)
+ - Root has deep leaves: A, B, C, D and CD (terminal leaves + intermediate leaves)
+
+ Arguments:
+ v is a node
+ n is a tree or a sub-tree
+
+ Returns
+ the similarity score
+ */
+function getElementS(v, n) {
+
+    var lv = v.deepLeafList;
+    var ln = n.deepLeafList;
+    var lvlen = lv.length;
+    var lnlen = ln.length;
+
+    var intersect = _.intersection(lv, ln).length;
+    return intersect / (lvlen + lnlen - intersect);
+
+}
+
+/*
+ Function that returns invisible children or visible children if one or the other are given as input
+ */
+function getChildren(d) {
+    return d._children ? d._children : (d.children ? d.children : []);
+}
+
+onmessage = function(event) {
+    var tree1 = event.data.tree1;
+    var tree2 = event.data.tree2;
+    var recalculate = event.data.recalculate;
+
+    // Instead of 2 calls to the same function, we should use web workers!
+    // BUT:
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=31666
+    wrapGetAllBCNs(tree1, tree2, recalculate);
+};

visualizer/js/canvas-toBlob.js
@@ -1,0 +1,125 @@
+/* canvas-toBlob.js
+ * A canvas.toBlob() implementation.
+ * 2016-05-26
+ * 
+ * By Eli Grey, http://eligrey.com and Devin Samarin, https://github.com/eboyjr
+ * License: MIT
+ *   See https://github.com/eligrey/canvas-toBlob.js/blob/master/LICENSE.md
+ */
+
+/*global self */
+/*jslint bitwise: true, regexp: true, confusion: true, es5: true, vars: true, white: true,
+  plusplus: true */
+
+/*! @source http://purl.eligrey.com/github/canvas-toBlob.js/blob/master/canvas-toBlob.js */
+
+(function(view) {
+"use strict";
+var
+	  Uint8Array = view.Uint8Array
+	, HTMLCanvasElement = view.HTMLCanvasElement
+	, canvas_proto = HTMLCanvasElement && HTMLCanvasElement.prototype
+	, is_base64_regex = /\s*;\s*base64\s*(?:;|$)/i
+	, to_data_url = "toDataURL"
+	, base64_ranks
+	, decode_base64 = function(base64) {
+		var
+			  len = base64.length
+			, buffer = new Uint8Array(len / 4 * 3 | 0)
+			, i = 0
+			, outptr = 0
+			, last = [0, 0]
+			, state = 0
+			, save = 0
+			, rank
+			, code
+			, undef
+		;
+		while (len--) {
+			code = base64.charCodeAt(i++);
+			rank = base64_ranks[code-43];
+			if (rank !== 255 && rank !== undef) {
+				last[1] = last[0];
+				last[0] = code;
+				save = (save << 6) | rank;
+				state++;
+				if (state === 4) {
+					buffer[outptr++] = save >>> 16;
+					if (last[1] !== 61 /* padding character */) {
+						buffer[outptr++] = save >>> 8;
+					}
+					if (last[0] !== 61 /* padding character */) {
+						buffer[outptr++] = save;
+					}
+					state = 0;
+				}
+			}
+		}
+		// 2/3 chance there's going to be some null bytes at the end, but that
+		// doesn't really matter with most image formats.
+		// If it somehow matters for you, truncate the buffer up outptr.
+		return buffer;
+	}
+;
+if (Uint8Array) {
+	base64_ranks = new Uint8Array([
+		  62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1
+		, -1, -1,  0, -1, -1, -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9
+		, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
+		, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35
+		, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
+	]);
+}
+if (HTMLCanvasElement && (!canvas_proto.toBlob || !canvas_proto.toBlobHD)) {
+	if (!canvas_proto.toBlob)
+	canvas_proto.toBlob = function(callback, type /*, ...args*/) {
+		  if (!type) {
+			type = "image/png";
+		} if (this.mozGetAsFile) {
+			callback(this.mozGetAsFile("canvas", type));
+			return;
+		} if (this.msToBlob && /^\s*image\/png\s*(?:$|;)/i.test(type)) {
+			callback(this.msToBlob());
+			return;
+		}
+
+		var
+			  args = Array.prototype.slice.call(arguments, 1)
+			, dataURI = this[to_data_url].apply(this, args)
+			, header_end = dataURI.indexOf(",")
+			, data = dataURI.substring(header_end + 1)
+			, is_base64 = is_base64_regex.test(dataURI.substring(0, header_end))
+			, blob
+		;
+		if (Blob.fake) {
+			// no reason to decode a data: URI that's just going to become a data URI again
+			blob = new Blob
+			if (is_base64) {
+				blob.encoding = "base64";
+			} else {
+				blob.encoding = "URI";
+			}
+			blob.data = data;
+			blob.size = data.length;
+		} else if (Uint8Array) {
+			if (is_base64) {
+				blob = new Blob([decode_base64(data)], {type: type});
+			} else {
+				blob = new Blob([decodeURIComponent(data)], {type: type});
+			}
+		}
+		callback(blob);
+	};
+
+	if (!canvas_proto.toBlobHD && canvas_proto.toDataURLHD) {
+		canvas_proto.toBlobHD = function() {
+			to_data_url = "toDataURLHD";
+			var blob = this.toBlob();
+			to_data_url = "toDataURL";
+			return blob;
+		}
+	} else {
+		canvas_proto.toBlobHD = canvas_proto.toBlob;
+	}
+}
+}(typeof self !== "undefined" && self || typeof window !== "undefined" && window || this.content || this));

visualizer/js/circular-json.js
@@ -1,0 +1,2 @@
+/*! (C) WebReflection Mit Style License */
+var CircularJSON=function(e,t){function l(e,t,o){var u=[],f=[e],l=[e],c=[o?n:"[Circular]"],h=e,p=1,d;return function(e,v){return t&&(v=t.call(this,e,v)),e!==""&&(h!==this&&(d=p-a.call(f,this)-1,p-=d,f.splice(p,f.length),u.splice(p-1,u.length),h=this),typeof v=="object"&&v?(a.call(f,v)<0&&f.push(h=v),p=f.length,d=a.call(l,v),d<0?(d=l.push(v)-1,o?(u.push((""+e).replace(s,r)),c[d]=n+u.join(n)):c[d]=c[0]):v=c[d]):typeof v=="string"&&o&&(v=v.replace(r,i).replace(n,r))),v}}function c(e,t){for(var r=0,i=t.length;r<i;e=e[t[r++].replace(o,n)]);return e}function h(e){return function(t,s){var o=typeof s=="string";return o&&s.charAt(0)===n?new f(s.slice(1)):(t===""&&(s=v(s,s,{})),o&&(s=s.replace(u,"$1"+n).replace(i,r)),e?e.call(this,t,s):s)}}function p(e,t,n){for(var r=0,i=t.length;r<i;r++)t[r]=v(e,t[r],n);return t}function d(e,t,n){for(var r in t)t.hasOwnProperty(r)&&(t[r]=v(e,t[r],n));return t}function v(e,t,r){return t instanceof Array?p(e,t,r):t instanceof f?t.length?r.hasOwnProperty(t)?r[t]:r[t]=c(e,t.split(n)):e:t instanceof Object?d(e,t,r):t}function m(t,n,r,i){return e.stringify(t,l(t,n,!i),r)}function g(t,n){return e.parse(t,h(n))}var n="~",r="\\x"+("0"+n.charCodeAt(0).toString(16)).slice(-2),i="\\"+r,s=new t(r,"g"),o=new t(i,"g"),u=new t("(?:^|([^\\\\]))"+i),a=[].indexOf||function(e){for(var t=this.length;t--&&this[t]!==e;);return t},f=String;return{stringify:m,parse:g}}(JSON,RegExp);

commit 3d50edc91d4c7479e15552ff251d0a10834dc55e

visualizer: Switch phylogenetic tree viewer to phylotree.js
In-browser phylogenetic tree viewing has been implemented since
462e2385f718 ("visualizer: Add rough phylo-io support") via the phylo-io
library. phylo-io could not be installed via npm, depended on critically
outdated versions of d3 and Bootstrap, and produced trees with poor visual
quality. Swap to phylotree.js for phylogenetics rendering, which
addresses each of these issues, while removing or upgrading
phylo-io's dependencies.

Fixes: 462e2385f718 ("visualizer: Add rough phylo-io support")
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

visualizer/_views/index.pug
@@ -3,172 +3,174 @@ extends layout
 block layout-content
   h1 #{refname} Results
   div.row
-    div.col-xs-2
-      div.panel.panel-primary
-        div.panel-heading
-          h2.panel-title Pipeline Results
-        div.panel-body
+    div.col-2
+      div.card.mb-3.mx-3
+        div.card-header
+          h2.card-title Pipeline Results
+        div.card-body
           ul
             li
               a(href="/multiqc") MultiQC Report
             li.dropdown
               a(href="/nf-report/latest") Nextflow Report
-              a.dropdown-toggle(href="#", data-toggle="dropdown")
+              a.dropdown-toggle(href="#", data-bs-toggle="dropdown")
                 span.caret
-              ul.dropdown-menu
+              .dropdown-menu
                 each report in nfreports
-                  li
-                    a(href=`/nf-report/${report.timestamp}`)= report.timestamp
+                  a(href=`/nf-report/${report.timestamp}`).dropdown-item= report.timestamp
             li.dropdown
               a(href="/nf-timeline/latest") Nextflow Timeline
-              a.dropdown-toggle(href="#", data-toggle="dropdown")
+              a.dropdown-toggle(href="#", data-bs-toggle="dropdown")
                 span.caret
-              ul.dropdown-menu
+              .dropdown-menu
                 each timeline in nftimelines
-                  li
-                    a(href=`/nf-timeline/${timeline.timestamp}`)= timeline.timestamp
+                  a(href=`/nf-timeline/${timeline.timestamp}`).dropdown-item= timeline.timestamp
             li.dropdown
               a(href="/nf-trace/latest") Nextflow Trace
-              a.dropdown-toggle(href="#", data-toggle="dropdown")
+              a.dropdown-toggle(href="#", data-bs-toggle="dropdown")
                 span.caret
-              ul.dropdown-menu
+              .dropdown-menu
                 each trace in nftraces
-                  li
-                    a(href=`/nf-trace/${trace.timestamp}`)= trace.timestamp
+                  a(href=`/nf-trace/${trace.timestamp}`).dropdown-item= trace.timestamp
             li.dropdown
               a(href="/nf-dag/latest") Nextflow Graph
-              a.dropdown-toggle(href="#", data-toggle="dropdown")
+              a.dropdown-toggle(href="#", data-bs-toggle="dropdown")
                 span.caret
-              ul.dropdown-menu
+              .dropdown-menu
                 each dag in nfdags
-                  li
-                    a(href=`/nf-dag/${dag.timestamp}`)= dag.timestamp
-      .panel.panel-primary
-        .panel-heading
-          h2.panel-title Reference Genome Results
-        .panel-body
+                  a(href=`/nf-dag/${dag.timestamp}`).dropdown-item= dag.timestamp
+      .card.mb-3.mx-3
+        .card-header
+          h2.card-title Reference Genome Results
+        .card-body
           ul
             li.dropdown
               a(href=`/reference/${refname}.fasta`)
-                span.glyphicon.glyphicon-download-alt
+                i.fas.fa-download
                 | &ensp; Download sequence
-              a.dropdown-toggle(href="#", data-toggle="dropdown")
+              a.dropdown-toggle(href="#", data-bs-toggle="dropdown")
                 span.caret
-              ul.dropdown-menu
-                li
-                  a(href=`/reference/${refname}.fasta.fai`)
-                    span.glyphicon.glyphicon-download-alt
+              .dropdown-menu
+                a(href=`/reference/${refname}.fasta.fai`).dropdown-item
+                    i.fas.fa-download
                     | &ensp; Download index
             li
               a(href=`/reference/${refname}.gff`)
-                span.glyphicon.glyphicon-download-alt
+                i.fas.fa-download
                 | &ensp; Download annotation
-    div.col-xs-10
-      div.panel.panel-primary
-        div.panel-heading
-          h2.panel-title Sample Results
-        div.panel-body
-          table.table.table-striped.table-responsive.table-hover
-            tr
-              th Sample Name
-              th Classification
-              th Alignment
-              th Assemblies
-              if platform == 'ont'
-                th Variant Calls
-              th Haplotypes
-              th Multiple Alignments
-              th Phylogenetics
-            each sample in samples
+    div.col-10
+      div.card.mb-3.mx-3
+        div.card-header
+          h2.card-title Sample Results
+        div.card-body
+          .table-responsive
+            table.table.table-striped.table-hover.table-sm
               tr
-                th=sample.samplename
-                td
-                  if haskraken
-                    ul
-                      li
-                        a(href=`/classification/${sample.samplename}.kreport`)
-                          span.glyphicon.glyphicon-download-alt
-                          | &nbsp; Report
-                      li
-                        a(href=`/classification/${sample.samplename}.kraken`)
-                          span.glyphicon.glyphicon-download-alt
-                          | &nbsp; Assignments
-                  else
-                    span.label.label-warning Read filtering skipped
-                td
-                  a(href=`/alignments/${sample.samplename}`)
-                    span.glyphicon.glyphicon-align-left
-                    | &nbsp; Interactive Viewer
-                  ul
-                    li.dropdown
-                      a(href=`/alignment/${sample.samplename}.bam`)
-                        span.glyphicon.glyphicon-download-alt
-                        | &nbsp; BAM File
-                      a(href="#", data-toggle="dropdown")
-                        span.caret
-                      ul.dropdown-menu
-                        li
-                          a(href=`/alignment/${sample.samplename}.bam.bai`)
-                            span.glyphicon.glyphicon-download-alt
-                            | &nbsp; BAM Index
-                td
-                  if sample.hascontigs
-                    ul
-                      li
-                        a(href=`/assembly/sequence/${sample.samplename}.contigs.fasta`)
-                          span.glyphicon.glyphicon-download-alt
-                          | &nbsp; Sequences
-                      li.dropdown
-                        a(href=`/assembly/alignment/${sample.samplename}.contigs.bam`)
-                          span.glyphicon.glyphicon-download-alt
-                          | &nbsp; Alignment
-                        a(href="#", data-toggle="dropdown")
-                          span.caret
-                        ul.dropdown-menu
-                          li
-                            a(href=`assembly/alignment/${sample.samplename}.contigs.bam.bai`)
-                              span.glyphicon.glyphicon-download-alt
-                              | &nbsp; BAM Index
-                  else
-                    span.label.label-warning
-                      i de novo
-                      |  assembly failed
+                th Sample Name
+                th Classification
+                th Alignment
+                th Assemblies
                 if platform == 'ont'
-                  td
-                    if sample.hasvariants
+                  th Variant Calls
+                th Haplotypes
+                th Multiple Alignments
+                th Phylogenetics
+              each sample in samples
+                tr
+                  th=sample.samplename
+                  if haskraken
+                    td
                       ul
                         li
-                          a(href=`/variants/${sample.samplename}.variants.tsv`)
-                            span.glyphicon.glyphicon-download-alt
-                            | &nbsp; Variant TSV
-                    else
-                      span.label.label-warning No variants table
-                td
-                  ul
-                    li
-                      a(href=`/haplotypes/${sample.samplename}.haplotypes.fasta`)
-                        span.glyphicon.glyphicon-download-alt
-                        | &nbsp; Sequences
-                      if platform == 'ont'
+                          a(href=`/classification/${sample.samplename}.kreport`)
+                            i.fas.fa-download
+                            | &nbsp; Report
                         li
-                          a(href=`/haplotypes/${sample.samplename}.haplotypes.yaml`)
-                            span.glyphicon.glyphicon-download-alt
-                            | &nbsp; Stats &amp; data
-                      else
-                        li
-                          a(href=`/haplotypes/${sample.samplename}.haplotypes.json`)
-                            span.glyphicon.glyphicon-download-alt
-                            | &nbsp; Stats &amp; data
-                td
-                  ul
-                    li
-                      a(href=`/multi_alignment/${sample.samplename}.haplotypes.fas`)
-                        span.glyphicon.glyphicon-download-alt
-                        | &nbsp; FASTA Alignment
-                td
-                  if sample.hastree
-                    a(href=`/phylogenetics/${sample.samplename}`)
-                      span.glyphicon.glyphicon-tree-conifer
-                      | &nbsp; Tree Viewer
+                          a(href=`/classification/${sample.samplename}.kraken`)
+                            i.fas.fa-download
+                            | &nbsp; Assignments
                   else
-                    span.label.label-warning No phylogenetic results
+                    td.table-warning
+                    i.fas.fa-exclamation
+                    | Read filtering skipped
+                  td
+                    a(href=`/alignments/${sample.samplename}`)
+                      i.fas.fa-stream
+                      | &nbsp; Interactive Viewer
+                    ul
+                      li.dropdown
+                        a(href=`/alignment/${sample.samplename}.bam`)
+                          i.fas.fa-download
+                          | &nbsp; BAM File
+                        a(href="#", data-bs-toggle="dropdown").dropdown-toggle
+                          span.caret
+                        .dropdown-menu
+                          a(href=`/alignment/${sample.samplename}.bam.bai`).dropdown-item
+                            i.fas.fa-download
+                            | &nbsp; BAM Index
+                    if sample.hascontigs
+                      td
+                        ul
+                          li
+                            a(href=`/assembly/sequence/${sample.samplename}.contigs.fasta`)
+                              i.fas.fa-download
+                              | &nbsp; Sequences
+                          li.dropdown
+                            a(href=`/assembly/alignment/${sample.samplename}.contigs.bam`)
+                              i.fas.fa-download
+                              | &nbsp; Alignment
+                            a(href="#", data-bs-toggle="dropdown").dropdown-toggle
+                              span.caret
+                            .dropdown-menu
+                              a(href=`assembly/alignment/${sample.samplename}.contigs.bam.bai`).dropdown-item
+                                i.fas.fa-download
+                                | &nbsp; BAM Index
+                    else
+                      td.table-warning
+                        i.fas.fa-exclamation
+                        | &nbsp;
+                        i de novo
+                        |  assembly failed
+                  if platform == 'ont'
+                      if sample.hasvariants
+                        td
+                          ul
+                            li
+                              a(href=`/variants/${sample.samplename}.variants.tsv`)
+                                i.fas.fa-download
+                                | &nbsp; Variant TSV
+                      else
+                        td.table-warning
+                          i.fas.fa-exclamation
+                          | &nbsp; No variants table
+                  td
+                    ul
+                      li
+                        a(href=`/haplotypes/${sample.samplename}.haplotypes.fasta`)
+                          i.fas.fa-download
+                          | &nbsp; Sequences
+                        if platform == 'ont'
+                          li
+                            a(href=`/haplotypes/${sample.samplename}.haplotypes.yaml`)
+                              i.fas.fa-download
+                              | &nbsp; Stats &amp; data
+                        else
+                          li
+                            a(href=`/haplotypes/${sample.samplename}.haplotypes.json`)
+                              i.fas.fa-download
+                              | &nbsp; Stats &amp; data
+                  td
+                    ul
+                      li
+                        a(href=`/multi_alignment/${sample.samplename}.haplotypes.fas`)
+                          i.fas.fa-download
+                          | &nbsp; FASTA Alignment
+                    if sample.hastree
+                      td
+                        a(href=`/phylogenetics/${sample.samplename}`)
+                          i.fas.fa-tree
+                          | &nbsp; Tree Viewer
+                    else
+                      td.table-warning
+                        i.fas.fa-exclamation
+                        | &nbsp; No phylogenetic results

visualizer/_views/layout.pug
@@ -4,13 +4,18 @@ html(lang="en")
   head
     meta(charset="utf-8")
     meta(name="viewport", content="width=device-width, initial-scale=1")
+    meta(http-equiv="X-UA-Compatible" content="IE=edge")
     link(rel="shortcut icon", href="/favicon.ico")
     link(rel="stylesheet", href="/css/twbs/bootstrap.min.css", type="text/css")
+    link(rel="stylesheet", href="/css/fa/all.min.css", type="text/css")
+    block layout-styles
     link(rel="stylesheet", href="/css/local/jev.css", type="text/css")
     title #{title} | JEV Analysis Pipeline Results
   body
     div.container-fluid
       block layout-content
     script(src="/js/jquery/jquery.slim.min.js")
+    script(src="/js/popper/popper.min.js")
     script(src="/js/twbs/bootstrap.min.js")
+    script(src="/js/fa/all.min.js")
     block layout-scripts

visualizer/_views/tree.pug
@@ -1,22 +1,42 @@
 extends layout
 
+block layout-styles
+  link(rel="stylesheet", href="/css/phylotree/phylotree.css", type="text/css")
+
 block layout-content
   h1 #{samplename}
+  div.group(role="group")
+    button(type="button", onClick="zoomin()").btn
+      i.fas.fa-search-plus
+    button(type="button" onClick="zoomout()").btn
+      i.fas.fa-search-minus
   div#phylotree
 
 block layout-scripts
-  script(src="/js/big-integer/BigInteger.min.js")
+  script(src="/js/d3/d3.js")
   script(src="/js/underscore/underscore-umd-min.js")
-  script(src="/js/spin/spin.min.js")
-  script(src="/js/d3/d3.min.js")
-  script(src="/js/cjson/circular-json.js")
-  script(src="/js/cblob/canvas-toBlob.js")
-  script(src="/js/filesaver/FileSaver.min.js")
-  script(src="https://cdn.jsdelivr.net/gh/DessimozLab/phylo-io@1.9/www/js/treecompare.js")
+  script(src="/js/lodash/lodash.min.js")
+  script(src="/js/phylotree/phylotree.js")
   script.
-    treecomp = TreeCompare().init({
-      treeHeight: 0.8*window.innerHeight
-    });
     newick = "#{sampletree}";
-    tree = treecomp.addTree(newick, undefined, 'single');
-    treecomp.viewTree(tree.name, 'phylotree');
+    tree = new phylotree.phylotree(newick);
+    rendered_tree = tree.render({
+      container: "#phylotree"
+    });
+    $(tree.display.container).empty();
+    $(tree.display.container).html(tree.display.show());
+
+    function zoom(amount) {
+      xzoom = tree.display.spacing_x.bind(tree.display);
+      yzoom = tree.display.spacing_y.bind(tree.display);
+      xzoom(xzoom() + amount).update();
+      yzoom(yzoom() + amount).update();
+    }
+
+    function zoomin(e) {
+      zoom(1);
+    }
+
+    function zoomout(e) {
+      zoom(-1);
+    }

visualizer/index.js
@@ -224,19 +224,20 @@ app.use('/multiqc_data', express.static(__dirname + '/multiqc_data'));
 // Javascript serving
 app.use('/js/igv', express.static(__dirname + '/node_modules/igv/dist'));
 app.use('/js/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
-app.use('/js/big-integer', express.static(__dirname + '/node_modules/big-integer'));
-app.use('/js/underscore', express.static(__dirname + '/node_modules/underscore'));
-app.use('/js/spin', express.static(__dirname + '/node_modules/spin.js'));
-app.use('/js/d3', express.static(__dirname + '/node_modules/d3'));
-app.use('/js/cjson', express.static(__dirname + '/node_modules/circular-json/build'));
-app.use('/js/cblob', express.static(__dirname + '/node_modules/canvas-toBlob'));
-app.use('/js/filesaver', express.static(__dirname + '/node_modules/file-saver'));
 app.use('/js/twbs', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js/popper', express.static(__dirname + '/node_modules/@popperjs/core/dist/umd'));
+app.use('/js/fa', express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/js'));
+app.use('/js/d3', express.static(__dirname + '/node_modules/d3/dist'));
+app.use('/js/underscore', express.static(__dirname + '/node_modules/underscore'));
+app.use('/js/lodash', express.static(__dirname + '/node_modules/lodash'));
+app.use('/js/phylotree', express.static(__dirname + '/node_modules/phylotree/dist'));
 
 // CSS Serving, from node_modules and locally
 app.use('/css/twbs', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-app.use('/css/fonts', express.static(__dirname + '/node_modules/bootstrap/dist/fonts'));
 app.use('/css/local', express.static(__dirname + '/_css'))
+app.use('/css/fa', express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/css'));
+app.use('/css/webfonts', express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/webfonts'));
+app.use('/css/phylotree', express.static(__dirname + '/node_modules/phylotree/dist'));
 
 // App startup
 const port = process.env.PORT || 3000;