c79be607ca939896c760f06719f3a5e607f8f132
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-01-24 12:56:14 -0600

    Add clade sorting buttons
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- visualizer/_js/phylotree-utils.js
+++ visualizer/_js/phylotree-utils.js
@@ -1,7 +1,23 @@
 $("[data-direction]").on("click", function(e) {
     var which_function =
-      $(this).data("direction") == "vertical"
+        $(this).data("direction") == "vertical"
         ? tree.display.spacing_x.bind(tree.display)
         : tree.display.spacing_y.bind(tree.display);
     which_function(which_function() + +$(this).data("amount")).update();
-  });
+});
+
+function sort_nodes(asc) {
+    tree.resortChildren(function(a, b) {
+        return (b.height - a.height || b.value - a.value) * (asc ? 1 : -1);
+    });
+}
+
+$("#sort-ascending").on("click", function(e) {
+    sort_nodes(true);
+    tree.display.update();
+});
+
+$("#sort-descending").on("click", function(e) {
+    sort_nodes(false);
+    tree.display.update();
+});
