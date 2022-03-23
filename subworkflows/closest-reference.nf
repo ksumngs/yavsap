ksumngs/yavsap

commit 18ed8a787353d90e1c72636fc976088a56a5e30f

Fix Nextflow using directory as a reference genome table
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

subworkflows/closest-reference.nf
@@ -17,7 +17,6 @@ workflow CLOSEST_REFERENCE {
 
     // Transform the TSV genome list into an edirect query
     genomeQuery = genome_list
-        .first()
         .readLines()
         .collect{ it.split('\t')[1] }
         .join(' OR ')

main.nf
@@ -153,7 +153,7 @@ workflow {
 
     // Find the strain genomes list
     genomePath = params.genome_list
-    genomeFile = file(genomePath)
+    genomeFile = file(genomePath, type: 'file')
     if (!genomeFile.toFile().exists()) {
         genomePath = "${workflow.projectDir}/genomes/${params.genome_list}*"
         genomeFile = file(genomePath, checkIfExists: true)

commit 25a458f4cd56dc35e15168d8f01e644da5189484

Fix calling readLines on list
Fixes: 18ed8a787353 ("Fix Nextflow using directory as a reference genome table")
Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

subworkflows/closest-reference.nf
@@ -17,6 +17,7 @@ workflow CLOSEST_REFERENCE {
 
     // Transform the TSV genome list into an edirect query
     genomeQuery = genome_list
+        .first()
         .readLines()
         .collect{ it.split('\t')[1] }
         .join(' OR ')