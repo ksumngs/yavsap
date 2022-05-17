6587f42116984b53c6a3dc5e0194d72c312bc1a1
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-05-17 17:12:59 -0500

    Add docs about new report format

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,11 @@ extensions = [
     "myst_parser",
 ]
 
+myst_enable_extensions = [
+    "strikethrough",
+    "deflist",
+]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
--- docs/index.md
+++ docs/index.md
@@ -12,4 +12,5 @@ install
 usage
 parameters
 output
+report
 ```
