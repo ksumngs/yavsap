ea7558354c2ba11345b381de838f3cff56bc15a7
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-05-17 17:45:00 -0500

    Remove unused static pages

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ html_logo = "images/yavsap_logo.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 intersphinx_mapping = {
     "nextflow": ("https://nextflow.io/docs/latest/", None),
