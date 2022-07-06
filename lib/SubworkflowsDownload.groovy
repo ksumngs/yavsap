460dbb1f818abbb70ee2c691ba7beef39c9971d7
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-07-06 11:07:38 -0500

    Add sortSequence function

--- lib/SubworkflowsDownload.groovy
+++ lib/SubworkflowsDownload.groovy
@@ -5,4 +5,22 @@
 
 class SubworkflowsDownload {
 
+    /**
+     * Gets the index that a fasta record occupies within a list of identifiers
+     *
+     * @param fastaText A string of a fasta record that the identifier will be parsed from
+     * @param accessionNumbers The list of identifiers that `fastaText` will be indexed against
+     * @return An integer index of where `fastaText` is found within `accessionNumbers`. If no match is found, returns -1.
+     */
+    public static Integer sortSequence(String fastaText, List accessionNumbers) {
+        String id = fastaText.split(' ')[0].replace('>', '')
+        Integer i = 0
+        for (accessionNumber in accessionNumbers) {
+            if (accessionNumber == id) {
+                return i
+            }
+            i += 1
+        }
+        return -1
+    }
 }
