a64bf71ef7dbc260d4de5a836a179daeed7fa007
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-07-06 11:32:38 -0500

    Add accessionKey function

--- lib/SubworkflowsDownload.groovy
+++ lib/SubworkflowsDownload.groovy
@@ -33,4 +33,20 @@ class SubworkflowsDownload {
     public static String accessionTransform(String[] accessionNumbers) {
         return accessionNumbers.join('|')
     }
+
+    /**
+     * Gets the combined accession number from a single accession number
+     *   and a list of list of accession numbers that are linked together
+     * @param id The accession number to get the key from
+     * @param accessionNumber A list of lists of linked accession numbers
+     */
+    public static String accessionKey(String id, List accessionNumbers) {
+        for (accessionNumber in accessionNumbers) {
+            String comboAccession = accessionTransform(accessionNumber[1])
+            if (comboAccession.contains(id)) {
+                return comboAccession
+            }
+        }
+        return null
+    }
 }
